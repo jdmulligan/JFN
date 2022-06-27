0f79c75af12b81887ca04fc31124ec8fd2dfb92e
jmulliga <james.mulligan@yale.edu>
2022-06-26 21:48:07 -0700

    add some setup for perlmutter

--- analysis/analyze_qg.py
+++ analysis/analyze_qg.py
@@ -48,6 +48,14 @@ from torch.nn import Linear
 
 os.environ['TORCH'] = torch.__version__
 print(torch.__version__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
 
 # Base class
 sys.path.append('.')
